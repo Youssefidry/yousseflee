fb50dd8f12c54935965c082d8e008ed08571dad8
Youssefidry <89986853+Youssefidry@users.noreply.github.com>
2023-02-11 18:33:37 +0100

    Add files via upload

--- js/switch-mode.js
+++ js/switch-mode.js
@@ -1,4 +1,4 @@
-const toggle = document.querySelector("#dark-mode-toggle");
+const toggle = document.querySelector("#dark-mode");
 toggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 });
